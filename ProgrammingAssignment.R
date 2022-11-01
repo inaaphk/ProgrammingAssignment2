ed174f71fc1c83439b29a9ed8365b69352a24fee
inaaphk <inaaphk@gmail.com>
2022-11-01 21:44:35 +0000

    Documentation for function missing, added information for completion.

--- ProgrammingAssignment.R
+++ ProgrammingAssignment.R
@@ -1,3 +1,8 @@
+## Caching the inverse of a Matrix
+
+## This function will create a special 'matrix' object that
+## can cache its inverse
+
 makeCacheMatrix <- function(m = matrix()) {
   i <- NULL
   set <- function(matrix){
@@ -16,6 +21,13 @@ makeCacheMatrix <- function(m = matrix()) {
   list(set = set, get = get, setInverse = setInverse,
        getInverse = getInverse)
 }
+
+## The next function computes the inverse of the
+## special 'matrix' returned by 'makeCacheMatrix'.
+## If the inverse was calculated previously, then
+## 'cacheSolve' will retrieve the inverse of the
+## cache.
+
 cacheSolve <- function (x,...) {
   m <- x$getInverse()
   if(!is.null(m)) {
